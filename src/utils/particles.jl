b03488cab3b5431f838ed979e459d2db8efd8337
Charles Knipp <charlesknipp98@gmail.com>
2023-12-09 19:19:54 -0500

    added features to ParticleContainer

--- src/utils/particles.jl
+++ src/utils/particles.jl
@@ -23,7 +23,27 @@ Particle(state::T) where {T} = Particle(Root(T), state)
 
 Base.show(io::IO, p::Particle{T}) where {T} = print(io, "Particle{$T}($(p.state))")
 
-const ParticleContainer{T} = AbstractVector{<:Particle{T}}
+"""
+    ParticleContainer{T}
+
+ParticleContainer is a weighted collection of Particles
+"""
+mutable struct ParticleContainer{T<:Particle}
+    vals::Vector{T}
+    log_weights::Vector{Float64}
+end
+
+function ParticleContainer(particles::Vector{<:Particle})
+    return ParticleContainer(particles, zeros(length(particles)))
+end
+
+Base.collect(pc::ParticleContainer) = pc.vals
+Base.length(pc::ParticleContainer) = length(pc.vals)
+Base.keys(pc::ParticleContainer) = LinearIndices(pc.vals)
+
+Base.@propagate_inbounds Base.getindex(pc::ParticleContainer, i::Int) = pc.vals[i]
+Base.@propagate_inbounds Base.getindex(pc::ParticleContainer, i::Vector{Int}) = pc.vals[i]
+Base.setindex!(pc::ParticleContainer{T}, p::T, i::Int) where T = Base.setindex!(pc.vals, p, i)
 
 """
     linearize(particle)
